67fc64380c944991e81c395b36f4e248a33dc504
Aiman Almureish <aiman.almureish@gmail.com>
2019-10-23 16:34:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function addingEvenetListener(){
+function addingEventListener(){
   const input = document.getElementById('input');
 input.addEventListener('click', function(event) {
   alert('I was clicked!');
